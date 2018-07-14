d752072583c876c8b5ee029d7a07493ae37ffbbf
Yuxuan Zhang <y.zhang.4@student.tue.nl>
2018-07-14 22:03:07 +0800

    Register arrival time of car added

--- Advanced Simulation - Assignment 4/src/simulation/entity/EntityManager.java
+++ Advanced Simulation - Assignment 4/src/simulation/entity/EntityManager.java
@@ -106,7 +106,8 @@ public class EntityManager implements Tick_Listener{
 		GenerateEntityCommand generateEntity = new GenerateEntityCommand(newEntity, startingSequenceObject);
 		
 		TimeManager.GetInstance().AddTimeEvent(new TimeEvent(timeOnWhichNextEntityArrives, generateEntity, String.format("Entity %s has arrived", newEntity.GetID()), Event_Type.GENERATE));
-			
+		newEntity.setArrivalTime(timeOnWhichNextEntityArrives);
+		System.out.println("[register arrival time of car]: "+ newEntity.GetID()+ "  "+newEntity.getArrivalTime());
 	}
 
 	
